c2529cb68ca377de8ed5d7daf2a3abb6d4a2b518
Taras Radulenko <Taras_Radulenko@epam.com>
2016-08-02 16:11:22 +0300

    update project 3

---
+++ TestMod-Child-2/src/test/java/tests/Test111_Test.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class Test111_Test {
+	@Test
+	public void test111(){
+		Logger log = Logger.getLogger(getClass());
+		log.info("Log from project 3");
+	}
+}
